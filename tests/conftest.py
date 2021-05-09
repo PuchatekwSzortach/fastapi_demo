34b6240d969308a8b75c01aa40c2137b40903a44
Jakub Kolodziejczyk <>
2021-05-09 23:22:23 +0900

    Fixed tests
    Added a bootstrap test fixture that creates user and some data (though user creation needs some improvements),
    tests now use authentication token

---
+++ tests/conftest.py
@@ -0,0 +1,73 @@
+"""
+Base module for pytest fixtures
+"""
+
+import os
+
+import fastapi.testclient
+import pytest
+
+import net.applications
+import net.globals
+
+
+@pytest.fixture(scope="function")
+def test_config() -> dict:
+    """
+    Get test configuration
+
+    Returns:
+        dict: test configuration
+    """
+
+    os.environ["CONFIG_PATH"] = "./configurations/test.yaml"
+    return net.globals.get_config()
+
+
+@pytest.fixture(scope="function")
+def bootstrap_user_data() -> tuple[fastapi.testclient.TestClient, str]:
+    """
+    Bootstrap user data:
+    - registers user
+    - obtains authentication token for user
+    - creates a few items
+
+    Returns:
+        tuple[fastapi.testclient.TestClient, str]: test client for the app, access token for registered user
+    """
+
+    app = net.applications.get_fastapi_app()
+    client = fastapi.testclient.TestClient(app)
+
+    response = client.post(
+        "/register",
+        json={
+            "email": "kuba@buba.com",
+            "password": "testtest"
+        }
+    )
+
+    response = client.post("/login", data={
+        "username": "kuba@buba.com",
+        "password": "testtest"
+    })
+
+    access_token = response.json()["access_token"]
+
+    items = [
+        {"name": "carrots", "age": "10"},
+        {"name": "beans", "age": "20"},
+        {"name": "eggs", "age": "1000"}
+    ]
+
+    for item in items:
+
+        response = client.post(
+            "/items",
+            json=item,
+            headers={"Authorization": f"Bearer {access_token}"}
+        )
+
+        assert response.status_code == 201
+
+    return client, access_token
