9b4e01d025bad3a31ae48601bdbebc7aa8f7697c
Jakub Kolodziejczyk <>
2021-05-20 21:22:32 +0900

    updated github actions

--- net/globals.py
+++ net/globals.py
@@ -4,7 +4,6 @@ Module with globals - because some of them you can't really avoid ><
 
 import functools
 import os
-import time
 
 import pydotenv
 import sqlalchemy_utils.functions
@@ -20,8 +19,6 @@ def get_config() -> dict:
         dict: configuration
     """
 
-    time.sleep(10)
-
     with open(os.environ["CONFIG_PATH"]) as file:
 
         config = yaml.safe_load(file)
@@ -29,18 +26,10 @@ def get_config() -> dict:
     # Load environment variables
     dot_environment = pydotenv.Environment(file_path=config["environment_variables_file"])
 
-    print("\n\n\nDot environment")
-    print(dot_environment.items())
-
     config["mysql_connection_string"] = (
         f"mysql://root:{dot_environment['mysql_password']}"
         f"@{dot_environment['mysql_container_name']}:3306/{config['mysql']['database_name']}"
     )
-
-    print("\n\n\n")
-    print("mysql connection string is")
-    print(config["mysql_connection_string"])
-    print("\n\n\n")
 
     # It's very dirty to create database in get_config.
     # This is just a temporary solution until we can hopefully move this responsibility to alembic
